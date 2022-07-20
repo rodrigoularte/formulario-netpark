da89d112aacc64cba6308fe7ca5f4f0aa938b5ff
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-07-20 19:00:26 -0300

    Recaptcha instalado e ok

--- submit.js
+++ submit.js
@@ -1,3 +1,23 @@
-const onSubmitForm = () => {
-  alert("Success!")
+// const onSubmitForm = () => {
+//   alert("Success!")
+// }
+
+document.getElementById("form-container").addEventListener("submit", function(event){
+  onSubmitForm(event)
+})
+
+
+function onSubmitForm(e) {
+  alert("teste")
+  console.log(e)
+  e.preventDefault()
+  grecaptcha.ready(function () {
+    grecaptcha.execute('6LdgLucgAAAAAGOn0l3Al1IXBGAQqIVQUvCkSAjJ', { action: 'submit' }).then(function (token) {
+      console.log("yusaguy")
+      console.log(token)
+      
+      console.log(grecaptcha.getResponse("form-container"))
+
+    })
+  })
 }
